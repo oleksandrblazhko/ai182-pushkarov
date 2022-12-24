9706aa2f193ca8bc7c7b1255ec7dfa423717b05a
Oleksii Pushkarov <41470575+ALlECoSAY@users.noreply.github.com>
2022-12-24 16:53:39 +0200

    Update change_data_secure.sql

--- Laboratory-work-8/change_data_secure.sql
+++ Laboratory-work-8/change_data_secure.sql
@@ -1,4 +1,4 @@
-create or replace function change_data(role_name VARCHAR, access_level INTEGER)
+create or replace function change_data(role_name VARCHAR, a_level INTEGER)
 returns integer as $$
 declare
 	str varchar;
@@ -6,6 +6,6 @@ begin
 	str := 'update role_access_level set access_level = $1 WHERE role_name = $2';
 	raise notice 'query=%', str;
 	EXECUTE str;
-    return str USING access_level, role_name;
+    return str USING a_level, role_name;
 end;
 $$ language plpgsql;
