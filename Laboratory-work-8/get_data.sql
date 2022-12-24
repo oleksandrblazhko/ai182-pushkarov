90ae2bd5f887212a361d098bd439b6dd7adbd8be
Oleksii Pushkarov <41470575+ALlECoSAY@users.noreply.github.com>
2022-12-24 15:16:06 +0200

    Update get_data.sql

--- Laboratory-work-8/get_data.sql
+++ Laboratory-work-8/get_data.sql
@@ -3,7 +3,7 @@ returns table (u_id INTEGER, name VARCHAR, year INTEGER) as $$
 declare
 str VARCHAR;
 begin
-str := 'SELECT u_id, name, year from university where name = ''' || university_name || '''';
+str := 'SELECT u_id, name, year from university where name = ''' || u_name || '''';
 raise notice 'Query=%', str;
 return query execute str;
 end;
