478427c3ddb67fffb394e5ceb4456055d1110bed
Oleksii Pushkarov <41470575+ALlECoSAY@users.noreply.github.com>
2022-12-24 16:55:15 +0200

    Update change_data_secure.sql

--- Laboratory-work-8/change_data_secure.sql
+++ Laboratory-work-8/change_data_secure.sql
@@ -5,7 +5,7 @@ declare
 begin
 	str := 'update role_access_level set access_level = $1 WHERE role_name = $2';
 	raise notice 'query=%', str;
-	EXECUTE str;
-    return str USING  access_level, role_name;
+	EXECUTE str USING access_level, role_name;
+    return access_level;
 end;
 $$ language plpgsql;
